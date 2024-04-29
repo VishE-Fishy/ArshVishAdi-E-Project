f1e9fb87ec566727959b56160df374cf2b9065a8
VishE-Fishy <157423243+VishE-Fishy@users.noreply.github.com>
2024-04-29 13:32:18 -0700

    Create login.py

---
+++ UI/Login UI/login.py
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: black;
+        }
+        div {
+            width: 320px;
+            height: 200px;
+            background-color:whitesmoke;
+            margin:center;
+            
+
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <p1>Username</p1>
+        <br>
+        <input type="text" id="user" name="usertextbox">
+        <br>
+        <p2>Password</p2>
+        <br>
+        <input type="number" id=""pass>
+        <br>
+        <button>LOGIN</button>
+    </div>
+</body>
+</html> 
