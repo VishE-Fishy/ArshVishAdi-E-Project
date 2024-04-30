20d6f5ae33c761d10006882dfbb3152541b77bac
ArshanAvula <157422387+ArshanAvula@users.noreply.github.com>
2024-04-30 13:57:16 -0700

    Update login.js

--- UI/Login UI/login.js
+++ UI/Login UI/login.js
@@ -4,6 +4,7 @@ var password = document.getElementById("pass")
 function login_button() {
     if (username.value == "AMVA" && password.value == 123){
         alert("Correct Login")
+        location.replace(src = "schedule.html")
     } else{
         alert("Wrong Login")
     }
