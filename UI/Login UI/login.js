6d73ab6264c872941f6a41c08e22add4a7c424ca
VishE-Fishy <157423243+VishE-Fishy@users.noreply.github.com>
2024-05-02 14:01:52 -0700

    Update login.js

--- UI/Login UI/login.js
+++ UI/Login UI/login.js
@@ -1,11 +1,17 @@
 var username = document.getElementById("user")
 var password = document.getElementById("pass")
+var userlabel = document.getElementById("userlabel")
+var passlabel = document.getElementById("passlabel")
+var loginbox = document.getElementById("loginbox")
 
 function login_button() {
-    if (username.value == "AMVA" && password.value == 123){
-        alert("Correct Login")
-        location.replace(src = "schedule.html")
-    } else{
-        alert("Wrong Login")
-    }
-}
+  if (username.value == "AMVA" && password.value == 123) {
+    passlabel.textContent = "Succsesful Login"
+    username.value = ""
+    password.value = ""
+    loginbox.style.borderColor = "black"
+  } else {
+    passlabel.textContent = "Incorrect Login. Your username or password is incorrect"
+    loginbox.style.borderColor = "red"
+  }
+  }
