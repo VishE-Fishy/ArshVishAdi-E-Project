86992fc4f11cb2901d685232283e71a055e3576f
Aditya-Jayanthi-CS <157422413+Aditya-Jayanthi-CS@users.noreply.github.com>
2024-05-09 19:45:49 -0700

    Update study.js

--- Study Page/study.js
+++ Study Page/study.js
@@ -2,55 +2,21 @@ function dashboard(){
     location.replace("dashboard.html")
 }
 
-var myVar = setInterval(function(){ myTimer() }, 1000);
-var secondlimit = document.getElementById("user_time").value;
-
-function myTimer() {
-if(secondlimit == 0)
-{
-    myStopFunction();
-}
-
-document.getElementById("safeTimerDisplay").innerHTML = '00:' + zeroPad(secondlimit,2);
-secondlimit = secondlimit  - 1;
+function message(){
+    alert("The timer has ended")
 
 }
-
-function myStopFunction() {
-    clearInterval(myVar);
-}
-
-function zeroPad(num, places) {
-  var zero = places - num.toString().length + 1;
-  return Array(+(zero > 0 && zero)).join("0") + num;
-}
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var time = 60 / 2, // your time in seconds here
-        display = document.querySelector('#safeTimerDisplay');
-    startTimer(time, display);
-};
-
 function start_timer(){
-    if (confirm("How many seconds would you like to input on your timer") == true) {
         let secondlimit = document.getElementById("user_time").value
+        var limit = secondlimit * 1000 + 1025
+        var downloadTimer = setInterval(function(){
+            if(secondlimit <= 0){
+                clearInterval(downloadTimer);
+                document.getElementById("countdown").innerHTML = "Finished";
+  }         else {
+                document.getElementById("countdown").innerHTML = secondlimit + " seconds remaining";
+  }
+            secondlimit -= 1;
+            }, 1000);
+  setTimeout(message, limit);
     }
-}
