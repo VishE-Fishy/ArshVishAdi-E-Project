03f9f6311abb0ef6f62c51cd4d33b0eccbf5abee
ArshanAvula <157422387+ArshanAvula@users.noreply.github.com>
2024-05-13 23:11:22 -0700

    Update study.js

--- Study Page/study.js
+++ Study Page/study.js
@@ -3,17 +3,17 @@ function dashboard(){
 }
 var timerDisplay;
 function timer(cancel) {
-      var countdown = document.getElementById("user_time").value; 
+      var countdown = document.getElementById("user_seconds").value; 
       if (cancel == 0) {
-        if (isNaN(parseInt(countdown))) {
-        document.getElementById('countdown').innerHTML = "Please input an integer"
-        alert("Please input an integer")
+        if (isNaN(countdown) || countdown % 1 != 0 || countdown < 0) {
+          document.getElementById('countdown').innerHTML = "Please type a positive whole number"
+          alert("Please type a positive whole number")
         } else {
           timerDisplay = setInterval(function() {
           document.getElementById("start_timer").disabled = true;
           document.getElementById("stop_timer").disabled = false;
           document.getElementById("reset_timer").disabled = false;
-          document.getElementById('user_time').value = countdown;
+          document.getElementById('user_seconds').value = countdown;
           if(countdown == 0){
               document.getElementById("countdown").innerHTML = "Timer has ended";
               clearInterval(timerDisplay);
@@ -40,7 +40,7 @@ function timer(cancel) {
           document.getElementById("start_timer").disabled = false;
           document.getElementById("stop_timer").disabled = true;
           document.getElementById("reset_timer").disabled = true;
-          document.getElementById('user_time').value = "";
+          document.getElementById('user_seconds').value = "";
           document.getElementById("countdown").innerHTML = "Timer has been reset"
       }
     }
