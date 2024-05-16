f59929612a53b29bbae7f1229b1a8fa79840b0fd
VishE-Fishy <157423243+VishE-Fishy@users.noreply.github.com>
2024-05-15 21:07:09 -0700

    Update study.js

--- Study Page/study.js
+++ Study Page/study.js
@@ -1,164 +1,101 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<style>
-    #countdown{
-        font-size: 70px;
+function dashboard(){
+  location.replace("dashboard.html")
+}
+var timerDisplay;
+var countdown = hours * 3600 + minutes * 60 + seconds * 1;
+var hours;
+var minutes;
+var seconds;
+function timer(cancel) {
+    var countdown = hours * 3600 + minutes * 60 + seconds * 1;
+    var hours = document.getElementById('user_hours').value
+    var minutes = document.getElementById('user_minutes').value
+    var seconds = document.getElementById('user_seconds').value 
+    if (cancel == 0) {
+      if (isNaN(hours) || hours % 1 != 0 || hours < 0) {
+        document.getElementById('countdown').innerHTML = "Please type a positive whole number"
+        alert("Please type a positive whole number")
+      }
+      else if (isNaN(minutes) || minutes % 1 != 0 || minutes < 0) {
+        document.getElementById('countdown').innerHTML = "Please type a positive whole number"
+        alert("Please type a positive whole number")
+      }
+      else if (isNaN(seconds) || seconds % 1 != 0 || seconds < 0) {
+        document.getElementById('countdown').innerHTML = "Please type a positive whole number"
+        alert("Please type a positive whole number")
+      }
+     else {
+      document.getElementById("start_timer").disabled = true;
+      document.getElementById("stop_timer").disabled = false;
+      document.getElementById("reset_timer").disabled = false;
+      document.getElementById('user_hours').value = hours
+      document.getElementById('user_minutes').value = minutes
+      document.getElementById('user_seconds').value = seconds
+      countdown = hours * 3600 + minutes * 60 + seconds * 1
+      timerDisplay = setInterval(function() {      
+      if(countdown == 0){
+          document.getElementById("countdown").innerHTML = "Timer has ended";
+          clearInterval(timerDisplay);
+          alert("Timer has ended")
+          document.getElementById("start_timer").disabled = false
+      } 
+      else{
+          document.getElementById('countdown').innerHTML = parseInt(countdown % (24*3600)/3600) + ":" + parseInt((countdown % 3600)/60) + ":" + parseInt(countdown%60);
+          document.getElementById('user_hours').value = parseInt(countdown % (24*3600)/3600);
+          document.getElementById('user_minutes').value = parseInt((countdown % 3600)/60);
+          document.getElementById('user_seconds').value = parseInt(countdown%60);
+          countdown--;
+      }      
+      }, 1000);
     }
-    #dashboardButton{
-        background-color:whitesmoke;
-        border-radius: 15px;
+  }
+    if (cancel == 1) {
+      clearInterval(timerDisplay);
+      document.getElementById("start_timer").disabled = false;
+      document.getElementById("stop_timer").disabled = true;
+      document.getElementById("reset_timer").disabled = false; 
+      alert("Timer has been stopped")  
+  }
+    if (cancel == 2) {
+      if (confirm("Are you sure you want to reset the timer?") == true){
+        clearInterval(timerDisplay);
+        document.getElementById("start_timer").disabled = false;
+        document.getElementById("stop_timer").disabled = true;
+        document.getElementById("reset_timer").disabled = true;
+        document.getElementById('user_seconds').value = "";
+        document.getElementById('user_hours').value = "";
+        document.getElementById('user_minutes').value = "";
+        document.getElementById("countdown").innerHTML = "Timer has been reset"
     }
-    #start_timer{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
+  }
+}
+var stopwatchDisplay;
+var counter = 0;
+function stopwatch(cancel) {
+  if (cancel == 0) {
+    document.getElementById("start_stopwatch").disabled = true
+    document.getElementById("stop_stopwatch").disabled = false
+    document.getElementById("reset_stopwatch").disabled = false
+      stopwatchDisplay = setInterval(function() {     
+          document.getElementById('stopwatch').innerHTML = parseInt(counter % (24*3600)/3600) + ":" + parseInt((counter % 3600)/60) + ":" + parseInt(counter%60);
+          counter++;
+      }, 1000);
+  }
+  if (cancel == 1) {
+    clearInterval(stopwatchDisplay)
+    document.getElementById("start_stopwatch").disabled = false
+    document.getElementById("stop_stopwatch").disabled = true
+    document.getElementById("reset_stopwatch").disabled = false    
+    alert("Stopwatch has been stopped")
+  }
+  if (cancel == 2) {
+    if (confirm("Are you sure you want to reset the stopwatch?") == true){
+      clearInterval(stopwatchDisplay)
+      document.getElementById("start_stopwatch").disabled = false
+      document.getElementById("stop_stopwatch").disabled = true
+      document.getElementById("reset_stopwatch").disabled = true
+      document.getElementById('stopwatch').innerHTML = "Stopwatch has been reset"
+      counter = 0;
     }
-    #stop_timer{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
-    }
-    #reset_timer{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
-    }
-    #dashboardButton:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #start_timer:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #stop_timer:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #reset_timer:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    body{
-        background-color: rgb(0, 0, 45);
-    }
-    #music{
-        margin-top: right;
-        border: 1px solid black;
-        text-align: center;
-    }
-    #start_stopwatch{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
-    }
-    #stop_stopwatch{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
-    }
-    #reset_stopwatch{
-        background-color:whitesmoke;
-        padding: 20px;
-        border-radius: 15px;
-    }
-    #start_stopwatch:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #stop_stopwatch:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #reset_stopwatch:hover{
-        transition: transform 1s;
-        transform: scale(1.1);
-    }
-    #stopwatch{
-        font-size: 70px;
-    }
-    #timerStopwatchCenter{
-        text-align: center;
-    }
-    table {
-      margin:auto;
-    }
-    .text {
-        color: whitesmoke;
-    }
-    .buttons {
-        background-color: whitesmoke;
-    }
-</style>
-<body>
-    <button id="dashboardButton" onclick="dashboard()">Back to Dashboard</button>
-    <div id="timerStopwatchCenter">
-        <div id="Timer">
-            <h2 class="text">Timer</h2>
-            <p id="TimerDisplay"></p>
-        </div>
-        <table style="width: 600px;">
-            <tr>
-                <td> <button class="buttons" id="start_timer" onclick="timer(0)">Start Timer</button></td>
-                <td><button class="buttons" id="stop_timer" onclick="timer(1)" disabled>Stop Timer</button></td>
-                <td><button class="buttons" id="reset_timer" onclick="timer(2)" disabled>Reset Timer</button></td>
-            </tr>       
-        </table>
-        <table style="width: 600px;">
-            <tr>
-                <th>Hours</th>
-                <th>Minutes</th>
-                <th>Seconds</th>
-            </tr>
-            <tr>
-                <td><input id="user_hours"></td>
-                <td><input id="user_minutes"></td>
-                <td><input id="user_seconds"></td>
-            </tr>
-            <tr>
-                <td style="text-align: center;" colspan="3"><label id="countdown"></label></td>
-            </tr>     
-        </table>
-        <br/>
-        <h2 class="text">Stopwatch</h2>
-        <table style="width: 600px;">
-            <tr>
-                <td><button class="buttons" id="start_stopwatch" onclick="stopwatch(0)" >Start Stopwatch</button></td>
-                <td><button class="buttons" id="stop_stopwatch" onclick="stopwatch(1)" disabled>Stop Stopwatch</button></td>
-                <td><button class="buttons" id="reset_stopwatch" onclick="stopwatch(2)" disabled>Reset Stopwatch</button></td>
-            </tr>
-            <tr>
-                <td style="text-align: center;" colspan="3"> 
-                    <label id="stopwatch"></label>
-                </td>
-            </tr>
-        </table>
-    </div>
-    </br>
-    </br>
-    <table id="music">
-        <tr id="music">
-            <th id="music"><a href="https://drive.google.com/file/d/1HotvRRpEBbumQ9_SpnYdDIdFkQPKqDHs/view?usp=sharing">1 A.M Study Session 📚 [lofi hip hop chill beats]</a></th>
-            <th id="music"><a href="https://drive.google.com/file/d/1AhPHfLkPxkpBBJEcfM-pbXPSMq-pCy-l/view?usp=sharing">study with me rainy season 🌨, lofi music, 1 hour</a></th>
-            <th id="music"><a href="https://drive.google.com/file/d/1NuxWMllknBLxnJWI8UfVjHvgsoZWZo2J/view?usp=sharing">1 Hour Of Aesthetic Lofi Music To Study Chill Sleep Relax</a></th>
-            <th id="music"><a href="https://drive.google.com/file/d/1JjRFZGWspTCKVOmQmHjcPo3prjoIxT-G/view?usp=sharing">Lo-fi Study Café 📖 1 Hour Chill Lo-fi No Ads ... u focus & study 🎧 Studying Music Work Aid</a></th>
-            <th id="music"><a href="https://drive.google.com/file/d/1jcFD6cO0y6ooNn_TnxHqkFuQ6xjDI4Nw/view?usp=sharing">3 A.M Study Session 📚 [lofi hip hop chill beats]</a></th>
-        </tr>
-        <tr id="music">
-            <td id="music"><audio controls autoplay><source src="1 A.M Study Session 📚 [lofi hip hop chill beats].mp3" type="audio/mpeg"></audio></td>
-            <td id="music"><audio controls autoplay><source src="study with me rainy season 🌨, lofi music, 1 hour.mp3" type="audio/mpeg"></audio></td>
-            <td id="music"><audio controls autoplay><source src="1 Hour Of Aesthetic Lofi Music To Study Chill Sleep Relax.mp3" type="audio/mpeg"></audio></td>
-            <td id="music"><audio controls autoplay><source src="Lo-fi Study Café 📖 1 Hour Chill Lo-fi No Ads to help you focus & study 🎧 Studying Music Work Aid.mp3" type="audio/mpeg"></audio></td>
-            <td id="music"><audio controls autoplay><source src="3 A.M Study Session 📚 [lofi hip hop chill beats].mp3" type="audio/mpeg"></audio></td>
-        </tr>
-    </table>
-</body>
-</html>
-<script src="script.js"></script>
-</body>
-</html>
+  }
+}
